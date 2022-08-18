11c479f2cc4846a6a64eab4da2e065b2504112cd
Raccoon-Necromancer <111468592+Raccoon-Necromancer@users.noreply.github.com>
2022-08-18 03:28:44 +0300

    Delete .github/workflows directory

--- .github/workflows/upload/fetch.py
+++
@@ -1,9 +0,0 @@
-import json
-import urllib3
-
-url = "https://api.github.com/repos/3Snake3/Sapphirium/releases/latest"
-http = urllib3.PoolManager()
-release_raw = http.request("GET", url).data
-release = json.loads(release_raw)
-tag = release["tag_name"]
-print(f"::set-output name=tag::{tag}")
